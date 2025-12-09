b0e487521cc214038a61bd0c63b4d9685627cc3b
Andres Flores <dflores.andres@gmail.com>
2025-12-08 22:33:56 -0500

    print fib sequence, added more comments

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -3,12 +3,17 @@ from collections import deque
 
 #  recursion: current iteration output is next iteration input
 def get_fibonacci(sequence:deque):
+
+    #  append sum to sequence deque
     sequence.append(sum(sequence))
+    print(sequence)
+
+    #  recursive calculation
     get_fibonacci(sequence)
 
 
 #  initialize deque container to keep O(1)
-sequence = deque(maxlen=2)
+sequence = deque(maxlen=2)  # limit to 2 items in a deque
 sequence.extend([0, 1])
 
 
